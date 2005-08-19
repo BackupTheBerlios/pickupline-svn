4f3ca459d102a81ff0ffdd176570d8e72c896186
deanalator <deanalator@8adbb26e-66fb-0310-a1f6-a2e85f718c82>
2005-08-19 05:07:24 +0000

    removing stuff

--- stable/cmd.clearinfo.c
+++
@@ -1,8 +0,0 @@
-#include"pul.h"
-
-int clearinfo(struct puldb* mydb)
-{
-	mydb->gotgwip=0;
-	mydb->gotgwmac=0;
-	mydb->gotnetmask=0;
-}
--- stable/cmd.dhcp.c
+++
@@ -1,8 +0,0 @@
-#include"pul.h"
-
-int dhcp(struct puldb* mydb)
-{
-	char buffer[40];
-	sprintf(buffer,"pump -i %s",mydb->interface);
-	system(buffer);;
-}
